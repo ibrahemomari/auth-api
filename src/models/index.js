21c5ee95836d54b37d89b8f713d364afa6818562
ibrahemomari <ibrahem.omari96@gmail.com>
2021-08-20 20:19:48 +0300

    fix deploy

--- src/models/index.js
+++ src/models/index.js
@@ -6,12 +6,20 @@ const foodModel = require("./food/model");
 const clothesModel = require("./clothes/model");
 const dataCollection = require("./data-collection");
 
+let sequelizeOptions = {
+      dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          }
+        }
+  };
 const DATABASE_URL =
   process.env.NODE_ENV == "test"
     ? "sqlite:memory"
     : process.env.DATABASE_URL;
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL,sequelizeOptions);
 const food = foodModel(sequelize, DataTypes);
 const clothes = clothesModel(sequelize, DataTypes);
 
